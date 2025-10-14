7ffd0564087b90d60a91d138048549446c05a534
smalllvcommm <3124944261@qq.com>
2025-10-14 19:55:19 +0800

    0

---
+++ Info/src/main/java/com/example/info/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.info.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
